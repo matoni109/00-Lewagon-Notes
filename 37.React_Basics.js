42a83a004f6fa97e101da6f4c32788eb1c5c99d2
Chris Wade <chris@wadespace.net>
2021-04-06 22:48:31 +1000

    updated

--- 37.React_Basics.js
+++ 37.React_Basics.js
@@ -1,20 +1,38 @@
-//
+// https://medium.com/@sherryhsu/es5-pseudoclassical-class-and-es6-es7-class-ac5bbbc13b93
 // Yarn Init
-
+// https://codepen.io/matoni109/pen/yLgJwLM
+// npx create-react-app my-app --scripts-version 1.1.5
 // cd ~/code/<your_github_nickname> mkdir yarn_project && cd $_ yarn init
 // use UNLICENSED when asked if private code
 
 // yarn add <package> [--dev]
 // yarn add eslint --dev
-eslint --init
-
+// eslint --init
+ "rules": {
+    "no-console": "off",
+    "comma-dangle": "off",
+    "quotes": "off",
+    "react/prop-types": 0,
+    "arrow-body-style": 0,
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+    "space-before-function-paren": 0
+  }
 //
 // touch .gitignore
-node_modules
+node_modules;
 // .eslintrc.json
 {
   "extends": "airbnb",
-    "rules": { "no-console": "off", "comma-dangle": "off", "quotes": "off", "react/prop-types": 0, "arrow-body-style": 0, "space-before-function-paren": 0 }, "env": { "browser": true }
+    "rules": { "no-console": "off",
+"comma-dangle": "off",
+  "quotes": "off",
+  "react/prop-types": 0,
+  "arrow-body-style": 0,
+  "space-before-function-paren": 0 },
+"env": { "browser": true }
 }
 
 const people = [
@@ -79,9 +97,18 @@ cities.some((city) => city.startsWith("P")); // true
 cities.every((city) => city.startsWith("P")); // false
 
 // Classes
+class Human {
+  constructor() {
+    this.gender = "male";
+  }
 
-class User {
+  printGender() {
+    console.log(this.gender);
+  }
+}
+class User extends Human {
   constructor(name, email) {
+    super();
     this.name = name;
     this.email = email;
   }
@@ -89,9 +116,119 @@ class User {
     return `Hello ${this.name}!`;
   }
 }
+// ES7
+class Human {
+  gender = "male";
+
+  printGender = () => {
+    console.log(this.gender);
+  };
+}
+class User extends Human {
+  constructor(name, email) {
+    super();
+    this.name = name;
+    this.email = email;
+  }
+  greet = () => {
+    `Hello ${this.name}!`;
+  };
+}
 
 // New instance of User
 const boris = new User("Boris", "boris@lewagon.org");
 console.log(boris.greet()); // Hello
+boris.printGender(); // "male"
 
+// Spread and Rest Opperator
+// ...
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4];
 
+console.log(newNumbers);
+
+const person = {
+  name: "max",
+};
+
+const newPerson = {
+  ...person,
+  age: 28,
+};
+console.log(newPerson);
+
+// [object Object] {
+//   age: 28,
+//   name: "max"
+// }
+
+// example
+const filter = (...args) => {
+  return args.filter((el) => el === 1);
+};
+
+console.log(filter(1, 2, 3));
+// [1]
+
+// Destructuring
+//
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+
+console.log(num1, num2); // 1 2
+//
+[num1, , num3] = numbers;
+console.log(num1, num3); // 1 3
+
+//  primitive type
+
+const number = 1;
+const num2 = number;
+
+console.log(num2); // 1
+
+// reference types
+
+const person = {
+  name: "Max",
+};
+
+const secondPerson = person;
+
+person.name = "Max";
+
+console.log(secondPerson); // Max
+
+// then if you change :
+
+const person = {
+  name: "Max",
+};
+
+const secondPerson = person;
+person.name = "Manu";
+
+console.log(secondPerson); // Manu
+
+// to fix
+const person = {
+  name: "Max",
+};
+
+const secondPerson = {
+  ...person,
+};
+person.name = "Manu";
+
+console.log(secondPerson); // Max
+
+/// array functions
+
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => {
+  return num * 2;
+});
+
+console.log(numbers);
+console.log(doubleNumArray);
